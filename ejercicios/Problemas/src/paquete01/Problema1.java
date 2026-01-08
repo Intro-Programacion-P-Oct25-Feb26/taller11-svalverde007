ba7bc03d9971a17c04852a7a3552d27b9a8b850b
svalverde007 <sdvalverde2@utpl.edu.ec>
2026-01-08 11:02:35 -0500

    Problema 01

--- ejercicios/Problemas/src/paquete01/Problema1.java
+++ ejercicios/Problemas/src/paquete01/Problema1.java
@@ -15,12 +15,26 @@ public class Problema1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion = {
+            {1, 2, 3},
+            {10, 20, 30},
+            {100, 200, 300}};
+        
         imprimirDatos(informacion);
     }
 
     public static void imprimirDatos(int[][] datos) {
-
+        String cadena = "";
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                
+                if(datos [i][j] %2 == 0){
+                    cadena = String.format("%s%s\n", cadena, datos[i][j]);
+                }
+                
+            }
+        }
+        System.out.printf(cadena);
     }
 
 }
