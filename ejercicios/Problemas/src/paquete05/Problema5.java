411af3ead7abe5bb6b6fe276fb32e2143538ebf0
svalverde007 <servio.valverde.uemv@gmail.com>
2026-01-09 18:09:50 -0500

    Problema 05

--- ejercicios/Problemas/src/paquete05/Problema5.java
+++ ejercicios/Problemas/src/paquete05/Problema5.java
@@ -5,10 +5,47 @@
  */
 package paquete05;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
+
 public class Problema5 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        int[][] informacion = {
+            {1, 2, 3}, 
+            {10, 20, 30}, 
+            {100, 200, 300}};
+        
+        int[][] informacion2 = {
+            {1, 2, 3},
+            {10, 20, 30}, 
+            {100, 200, 300}};
+        
+        int[][] matriz = sumarMatrices(informacion, informacion2);
+ 
+        for (int[] matriz1 : matriz) {
+            for (int j = 0; j < matriz1.length; j++) {
+                System.out.printf("%s\n", matriz1[j]);
+            }  
+        }
+    }
+    public static int[][] sumarMatrices(int[][] m1, int[][] m2) {
+        int filas = m1.length;
+        int columnas = m1[0].length;
+        int[][] suma = new int[filas][columnas];
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                suma[i][j] = m1[i][j] + m2[i][j];
+            }
+        }
+        return suma;
+    }
 }
