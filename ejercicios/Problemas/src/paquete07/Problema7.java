eb7b80a6cd42cf8fe277c7cd7f399b4abff06da3
svalverde007 <servio.valverde.uemv@gmail.com>
2026-01-09 20:17:27 -0500

    Problema 07

--- ejercicios/Problemas/src/paquete07/Problema7.java
+++ ejercicios/Problemas/src/paquete07/Problema7.java
@@ -9,6 +9,47 @@ package paquete07;
  *
  * @author reroes
  */
+
+import java.util.Scanner;
+
 public class Problema7 {
+
+    public static void main(String[] args) {
+
+        String[] ciudadesEcuador = ciudades();
+
+        System.out.println("Ciudades ingresadas:");
+        for (int i = 0; i < ciudadesEcuador.length; i++) {
+            System.out.println("- " + ciudadesEcuador[i]);
+        }
+        System.out.println("\nCiudades con 4 o 5 caracteres:");
+        ciudadesMenoresCinco(ciudadesEcuador);
+    }
+
+    public static String[] ciudades() {
+        Scanner entrada = new Scanner(System.in);
+        int numeroCiudades;
+        String nombres;
+
+        System.out.println("Ingrese el numero de ciudades");
+        numeroCiudades = entrada.nextInt();
+        entrada.nextLine();
+        String[] ciudades = new String[numeroCiudades];
+
+        for (int i = 0; i < numeroCiudades; i++) {
+            System.out.println("Ingrese el nombre de la ciudad numero: " + (i + 1));
+            nombres = entrada.nextLine();
+            ciudades[i] = nombres;
+        }
+        return ciudades;
+    }
     
+     public static void ciudadesMenoresCinco(String[] ciudadesEcuador) {
+         
+          for (int i = 0; i < ciudadesEcuador.length; i++) {
+            if((ciudadesEcuador[i].length() == 4) || (ciudadesEcuador[i].length() == 5)){
+                  System.out.println(ciudadesEcuador[i]); 
+            }
+        }
+     }
 }
